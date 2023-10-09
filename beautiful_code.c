396c3362cea6cc1dda78d7f40510d324be483149
lain0x0 <lainyamato03@gmail.com>
2023-10-09 14:22:09 -0700

    Betty would be proud

---
+++ beautiful_code.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry Point
+ * @argc: Argument Counter
+ * @argv: Argument Vector
+ * @envp: Environment variables
+ *
+ * Description: This program prints the process ID (PID).
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[], char *envp[])
+{
+	(void)argc;
+
+	int pid_t;
+
+	pid_t = getpid();
+
+	printf("The Process ID is: %d\n", pid_t);
+
+	return (0);
+}
+
