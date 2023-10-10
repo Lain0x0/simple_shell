eba5321efe45d25159b3a2cf54cc687fc4cb8401
lain0x0 <lainyamato03@gmail.com>
2023-10-10 13:16:59 -0700

    Betty Should Be Proud

--- shell.h
+++ shell.h
@@ -1,10 +1,16 @@
 #ifndef SHELL_H
 #define SHELL_H
-#define MAX_INPUT_SIZE 1024
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define MAX_INPUT_SIZE 1024
+#define MAX_BUFFER_SIZE 1024
+#define MAX_COMMAND_LENGTH 100
+
 
 void run_shell();
 
