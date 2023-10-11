8bf0d449fc0b74ec5cf72288d5795fd51d45c405
lain0x0 <lainyamato03@gmail.com>
2023-10-11 13:02:54 -0700

    Unix Shell

---
+++ unix.c
@@ -0,0 +1,102 @@
+#include "shell.h"
+
+/**
+ * main - Entry Point
+ *
+ * execute_command - Execute a command in a child process
+ *
+ * @args : An array of strings containing the command
+ *
+ * Description : shell by using c #2
+ *
+ * Return : Always 0 (SUCCESS)
+ *
+ */
+
+
+void execute_command(char *args[])
+{
+	pid_t pid, wpid;
+
+	int status;
+
+	pid = fork();
+
+	if (pid == 0)
+
+	{
+		if (execvp(args[0], args) == -1)
+
+		{
+			perror("execvp");
+		}
+
+		exit(EXIT_FAILURE);
+	}
+
+	else if (pid < 0)
+
+	{
+		perror("fork");
+	}
+
+	else
+
+	{
+
+		do
+
+		{
+			wpid = waitpid(pid, &status, WUNTRACED);
+		}
+
+		while (!WIFEXITED(status) && !WIFSIGNALED(status));
+
+	}
+}
+
+int main(void)
+{
+	char input[MAX_INPUT_LENGTH];
+	char *args[MAX_ARG_COUNT];
+	char *token;
+	int arg_count;
+
+	while (1)
+	{
+		printf("$ ");
+		fgets(input, MAX_INPUT_LENGTH, stdin);
+
+
+		input[strcspn(input, "\n")] = '\0';
+
+		token = strtok(input, " ");
+		arg_count = 0;
+
+		while (token != NULL)
+
+		{
+			args[arg_count] = token;
+			arg_count++;
+
+			token = strtok(NULL, " ");
+		}
+
+		args[arg_count] = NULL;
+
+		if (arg_count > 0)
+
+		{
+			if (strcmp(args[0]
+						, "exit") == 0)
+
+			{
+				exit(0);
+			}
+
+			execute_command(args);
+		}
+	}
+
+	return (0);
+}
